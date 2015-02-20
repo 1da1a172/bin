a2d67441d5ada85866d178c1a390feb16c428c67
Jonathan Waldrep <waldrep@vt.edu>
2015-02-20 13:52:08 -0500

    merged write_profiles() and write_unnamed_profiles(); sed optimizations

--- filize-aos.zsh
+++ filize-aos.zsh
@@ -10,34 +10,23 @@ function write_profiles() {
                      # Making this an array makes is easy to pull off a quoted profile name with a space
 
     definition=$1
-    folder="$PREFIX"/$( echo "$definition" | sed 's: :/:g' ) # note that sed is deliminated with : not / for readability
     defline=( $( sed -n "/^$definition/=" $CONFIG ) )
 
-    [[ -d $folder ]] && rm -r $folder
+    folder="$PREFIX"/$( echo "$definition" | sed 's: :/:g' ) # note that sed is deliminated with : not / for readability
+    [[ -e $folder ]] && rm -r $folder # at this point, this could be a folder containing multiple profiles or a single file for an unnamed profile
+    [[ "$( sed -n "$defline[1]{p;q}" $CONFIG )" == "$definition" ]] && folder=$( dirname $folder ) # adjust if unnamed profile
+
     [[ ${#defline} -eq 0 ]] && { echo "No \"$definition\" defined" >&2 ; return }
     mkdir -p $folder
 
     for i in {1..${#defline}} ; do
         # name the output file after the profile name
-        defstr=( $(sed -n $defline[i]p $CONFIG) )
-        file=$folder/$( echo $defstr[${#defstr}] | sed s/\"//g )
+        defstr=( $(sed -n "$defline[i]{p;q}" $CONFIG) )
+        file=$folder/$( echo $defstr[${#defstr}] | sed 's/"//g' )
 
         # Parse for raw config of ith profile and write to $file
-        sed -n "$defline[i],/^!$/p" $CONFIG | sed 1d | sed '$d' | sed 's/^ *//' > $file
+        sed -n "$defline[i]{:loop n; /^!$/q; s/^ *//p; b loop}" $CONFIG > $file
     done
-}
-
-function write_unnamed_profile() {
-    local integer i
-    local definition      # keyword string to declare the profile in AOS
-    local file            # where on the disk we are going to write the file. Instance in this function includes path
-
-    definition=$1
-    file="$PREFIX"/$( echo "$definition" | sed 's: :/:g' ) # note that sed is deliminated with : not / for readability
-
-    mkdir -p $( dirname $file )
-
-    sed -n "/^$definition/,/^!$/p" $CONFIG | sed 1d | sed '$d' | sed 's/^ *//' > $file
 }
 
 function write_option_group() {
@@ -46,8 +35,7 @@ function write_option_group() {
 
     definition=$1
     file="$PREFIX"/$( echo "$definition" | sed 's: :/:g' ) # note that sed is deliminated with : not / for readability
-    grep "^$definition" $CONFIG | cut -d ' ' -f $[ $( echo $definition | wc -w ) + 1 ]- > $file # grep config for any line that starts with the definition, then write only the part of the line after the definition to the file.
-
+    sed -n "/^$definition/ s/$definition //p" $CONFIG > $file
 }
 
 CONFIG=$1
@@ -73,13 +61,10 @@ for type in conn-capability op-cl operator-friendly-name wan-metrics
 
 ## aaa ###
 # TODO dns-query-interval log "radius-attributes add" "tacacs-accounting server-group" "user {fast-age,stats-poll}" "xml-api server"
-for type in alias-group bandwidth-contract profile rfc-3576-server server-group "derivation-rules user"
+for type in alias-group bandwidth-contract "derivation-rules user" "password-policy mgmt" profile rfc-3576-server server-group
     write_profiles "aaa $type"
-write_unnamed_profile "aaa password-policy mgmt"
-for type in captive-portal dot1x mac mgmt stateful-{kerberos,ntlm} vpn wispr
+for type in captive-portal dot1x mac mgmt stateful-{kerberos,ntlm,dot1x} vpn wired wispr
     write_profiles "aaa authentication $type"
-for type in stateful-dot1x wired
-    write_unnamed_profile "aaa authentication $type"
 for type in ldap radius tacacs windows # TODO "internal use-local-switch"
     write_profiles "aaa authentication-server $type"
 write_option_group "aaa timers"
